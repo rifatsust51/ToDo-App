eb50059c73755bb0654f210c31439ccabbcca11c
rifatsust51 <61629666+rifatsust51@users.noreply.github.com>
2022-12-20 01:20:08 +0600

    Create app.js

---
+++ Src/app.js
@@ -0,0 +1,43 @@
+
+
+
+
+import React from "react";
+import "./App.css";
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+
+
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          
+    
+    <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
